60e06976c98e3933062e769a6ba60c380e889a90
Pranavi2200030607 <145761882+Pranavi2200030607@users.noreply.github.com>
2024-12-22 22:55:01 +0530

    adding files for code deploy

--- Ecommerce/scripts/start_instance.sh
+++ Ecommerce/scripts/start_instance.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-echo "Starting instance services..."
-# Start specific services
-sudo systemctl start apache2   # For Ubuntu
-sudo systemctl start httpd     # For Amazon Linux/RedHat
 
-# Add additional start commands as needed
-echo "Services started successfully."
+set -e
+
+# Clone the Spring Boot project repository
+git clone https://github.com/Pranavi2200030607/SpringBoot.git
+
+# Navigate to the project directory
+cd Ecommerce
+
+# Build the Spring Boot application using Maven
+mvn clean install
+
+# Run the Spring Boot application
+java -jar target/Ecommerce-0.0.1-SNAPSHOT.jar --server.port=8000
--- Ecommerce/scripts/stop_instance.sh
+++ Ecommerce/scripts/stop_instance.sh
@@ -1,8 +1,3 @@
-#!/bin/bash
-echo "Stopping instance services..."
-# Stop specific services
-sudo systemctl stop apache2   # For Ubuntu
-sudo systemctl stop httpd     # For Amazon Linux/RedHat
+set -e
 
-# Add additional stop commands as needed
-echo "Services stopped successfully."
+echo "HI"
