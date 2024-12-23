66cba501488ca1aa855518a38a3ec55a38de9a01
Pranavi2200030607 <145761882+Pranavi2200030607@users.noreply.github.com>
2024-12-23 15:24:47 +0530

    Update start_instance.sh

--- scripts/start_instance.sh
+++ scripts/start_instance.sh
@@ -6,7 +6,7 @@ set -e
 git clone https://github.com/Pranavi2200030607/SpringBoot.git
 
 # Navigate to the project directory
-cd SpringBoot
+cd ..
 cd Ecommerce
 
 # Build the Spring Boot application using Maven
