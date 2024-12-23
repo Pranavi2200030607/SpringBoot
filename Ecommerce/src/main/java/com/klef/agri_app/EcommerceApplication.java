28c0f6549b267fafc3f12fac666366ea5d8e5a57
Pranavi2200030607 <145761882+Pranavi2200030607@users.noreply.github.com>
2024-12-23 23:00:23 +0530

    modified

--- Ecommerce/src/main/java/com/klef/agri_app/EcommerceApplication.java
+++ Ecommerce/src/main/java/com/klef/agri_app/EcommerceApplication.java
@@ -12,7 +12,7 @@ public class EcommerceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceApplication.class, args);
 		
-		System.out.println("project is working fine and good");
+		System.out.println("project is working fine");
 		
 	}
 	
